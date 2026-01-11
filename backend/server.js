5081f90a37df1d3ee5bb26072191b6414c4201a7
Isaac Apraku Amankwah <isaacaprakuamankwah001@gmail.com>
2026-01-11 16:30:32 +0000

    API Summary endpoint

--- backend/server.js
+++ backend/server.js
@@ -82,7 +82,34 @@ app.delete("/api/transactions/:id", async(req, res)=>{
     res.status(200).json({ message: "Transaction deleted successfully" });
   } catch (error) {
     console.log("Error deleting the transaction: ", error);
-    res.status(500).json({message: "Internal Server Error"})
+    res.status(500).json({message: "Internal Server Error"});
+  }
+})
+
+app.get("/api/transactions/summary/:userId", async(req,res)=>{
+  try {
+    const {userId} = req.params;
+
+    const balanceResult = await db`
+      SELECT COALESCE(SUM(amount), 0) as balance FROM transactions WHERE user_id = ${userId}
+    `
+    const incomeResult = await db`
+      SELECT COALESCE(SUM(amount), 0) as income FROM transactions 
+      WHERE user_id = ${userId} AND amount > 0
+    `
+    const expensesResult = await db`
+      SELECT COALESCE(SUM(amount), 0) as expenses FROM transactions 
+      WHERE user_id = ${userId} AND amount < 0
+    `
+    res.status(200).json({
+      balance : balanceResult[0].balance,
+      income : incomeResult[0].income,
+      expenses : expensesResult[0].expenses,
+    })
+    
+  } catch (error) {
+    console.log("Error getting the summary: ", error);
+    res.status(500).json({message: "Internal Server Error"});
   }
 })
 
