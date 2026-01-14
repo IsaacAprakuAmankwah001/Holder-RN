e60d935ca5648f8c295cdb833ef3719edccc5b50
Isaac Apraku Amankwah <isaacaprakuamankwah001@gmail.com>
2026-01-14 22:30:32 +0000

    Debug

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { db } from './config/db.js';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
 const app = express();
 //built-in middleware
-app.use(express.json())
+app.use(rateLimiter);
+app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 
