78c03aa495fc1661c68b1d128f154a8deb832e5c
Isaac Apraku Amankwah <isaacaprakuamankwah001@gmail.com>
2026-01-23 14:55:06 +0000

    Added getTransactionbyId

---
+++ backend/controllers/transactionsController.js
@@ -0,0 +1,15 @@
+async function getTransactionByUserId() {
+    async(req, res)=>{
+        try {
+            const{userId} =  req.params;
+    
+            const transactions = await db`
+                SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC
+            `;
+         res.status(200).json(transactions);
+        } catch (error) {
+            console.log("Error getting the transaction: ", error);
+            res.status(500).json({message: "Internal Server Error"});
+        }
+    }
+}
