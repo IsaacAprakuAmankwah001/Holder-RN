5d6f1017376a8be0d9e7e9a7a4bfdeca6a0ed9f2
Isaac Apraku Amankwah <isaacaprakuamankwah001@gmail.com>
2026-01-26 23:48:35 +0000

    Error Fixed

--- backend/config/upstash.js
+++ backend/config/upstash.js
@@ -4,7 +4,7 @@ import {Ratelimit} from '@upstash/ratelimit'
 import 'dotenv/config';
 
 const ratelimit = new Ratelimit({
-  redis: Redis.fromEnv(),
+  redis: Redis.fromEnv(), 
   limiter:Ratelimit.slidingWindow(100,'60 s'),
 })
  
