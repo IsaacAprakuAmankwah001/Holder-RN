cfde1e38cf65a8b25de218edae418de41d020685
Isaac Apraku Amankwah <isaacaprakuamankwah001@gmail.com>
2026-01-28 23:40:04 +0000

    Removed create Transaction

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,29 +1,11 @@
 import express from "express";
 import {db} from "../config/db.js";
-import {getTransactionByUserId} from "../controllers/transactionsController.js"
+import {createTransaction, getTransactionByUserId} from "../controllers/transactionsController.js"
 
 
 const router = express.Router();
 
-router.post("/", async(req, res)=>{
-  try{
-    const{title, amount, category, user_id} = req.body;
-
-    if (!title || !category || !user_id || amount == undefined) {
-      return res.status(400).json({message: "All fields are required"});
-    }
-    const transaction =  await db`
-      INSERT INTO transactions(user_id, title, amount, category)
-      VALUES(${user_id}, ${title}, ${amount}, ${category})
-      RETURNING *
-    `
-    console.log(transaction);
-    res.status(201).json(transaction[0])
-  } catch(error){
-    console.log("Error creating the transaction: ", error);
-    res.status(500).json({message: "Internal Server Error"});
-  }
-});
+router.post("/", createTransaction);
 
 router.get("/:userId", getTransactionByUserId);
 
