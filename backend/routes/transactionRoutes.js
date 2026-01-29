2c29e6c45839e8b6f9c09a1f9f34a1c1daf043f9
Isaac Apraku Amankwah <isaacaprakuamankwah001@gmail.com>
2026-01-29 22:53:19 +0000

    Get summary by ID

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {db} from "../config/db.js";
-import {createTransaction, deleteTransaction, getTransactionByUserId} from "../controllers/transactionsController.js"
+import {createTransaction, deleteTransaction, getSummaryByUserId, getTransactionByUserId} from "../controllers/transactionsController.js"
 
 
 const router = express.Router();
@@ -11,31 +11,6 @@ router.get("/:userId", getTransactionByUserId);
 
 router.delete("/:id", deleteTransaction)
 
-router.get("/:userId", async(req,res)=>{
-  try {
-    const {userId} = req.params;
-
-    const balanceResult = await db`
-      SELECT COALESCE(SUM(amount), 0) as balance FROM transactions WHERE user_id = ${userId}
-    `
-    const incomeResult = await db`
-      SELECT COALESCE(SUM(amount), 0) as income FROM transactions 
-      WHERE user_id = ${userId} AND amount > 0
-    `
-    const expensesResult = await db`
-      SELECT COALESCE(SUM(amount), 0) as expenses FROM transactions 
-      WHERE user_id = ${userId} AND amount < 0
-    `
-    res.status(200).json({
-      balance : balanceResult[0].balance,
-      income : incomeResult[0].income,
-      expenses : expensesResult[0].expenses,
-    })
-    
-  } catch (error) {
-    console.log("Error getting the summary: ", error);
-    res.status(500).json({message: "Internal Server Error"});
-  }
-})
+router.get("/:userId", getSummaryByUserId)
 
 export default router;
