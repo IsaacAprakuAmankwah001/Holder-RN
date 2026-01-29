bdeec6ab350595daa88ae87a9b79bd056ac4e466
Isaac Apraku Amankwah <isaacaprakuamankwah001@gmail.com>
2026-01-29 22:54:29 +0000

    Added  getSummaryUserId

--- backend/controllers/transactionsController.js
+++ backend/controllers/transactionsController.js
@@ -55,3 +55,30 @@ export async function deleteTransaction(req, res){
     res.status(500).json({message: "Internal Server Error"});
   }
 }
+
+export async function getSummaryByUserId(req,res){
+  try {
+    const {userId} = req.params;
+
+    const balanceResult = await db`
+      SELECT COALESCE(SUM(amount), 0) as balance FROM transactions WHERE user_id = ${userId}
+    `
+    const incomeResult = await db`
+      SELECT COALESCE(SUM(amount), 0) as income FROM transactions 
+      WHERE user_id = ${userId} AND amount > 0
+    `
+    const expensesResult = await db`
+      SELECT COALESCE(SUM(amount), 0) as expenses FROM transactions 
+      WHERE user_id = ${userId} AND amount < 0
+    `
+    res.status(200).json({
+      balance : balanceResult[0].balance,
+      income : incomeResult[0].income,
+      expenses : expensesResult[0].expenses,
+    })
+    
+  } catch (error) {
+    console.log("Error getting the summary: ", error);
+    res.status(500).json({message: "Internal Server Error"});
+  }
+}
