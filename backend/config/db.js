80b49de3b9528dc6a0b66ab81935e4711eeff9e2
Isaac Apraku Amankwah <isaacaprakuamankwah001@gmail.com>
2026-01-31 23:05:40 +0000

    Added initDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,3 +3,22 @@ import "dotenv/config";
 
 //Creates a SQL connection using  the database URL
 export const db = neon(process.env.DATABASE_URL);
+
+export async function initDBConnection() {
+  try {
+    await db`CREATE TABLE IF NOT EXISTS transactions(
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount DECIMAL(10, 2) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        );`
+        // DECIMAL(10,2) a fixed-pointer number with:
+        // 10 digits total & 2 digits after the decimal point so max value is 99999999.99
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.error('Error initializing DB:', error);
+    process.exit(1) // status code 1 means failure, 0 for success
+  }
+}
